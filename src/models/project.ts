60f8cbf40cbbebf1dcd061b6841f89a2d2731818
tttfff <104338839+anjiazhuyouxing@users.noreply.github.com>
2023-07-28 12:58:45 +0800

    add:models (#1)

---
+++ src/models/project.ts
@@ -0,0 +1,39 @@
+import { Types } from "mongoose";
+
+const ProjectSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  description: {
+    type: String,
+    required: true,
+  },
+
+  created_by: {
+    type: Types.ObjectId, //ref userid
+    ref: "user",
+    required: true,
+  },
+
+  members: [
+    {
+      member: {
+        type: Types.ObjectId,
+        ref: "user", // 指向 User 模型的引用
+        required: true,
+      },
+
+      //0：管理 1：读写 2：只读
+      permission: {
+        type: Number,
+        required: true,
+        default: 2,
+      },
+    },
+  ],
+});
+
+const projectModel = mongoose.model("project", ProjectSchema, "projects");
+module.exports = projectModel;
