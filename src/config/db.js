2e62b807fd9a1ab4ef7147a5f74c07c21143eb0c
DragonnZhang <731557579@qq.com>
2023-07-28 16:23:44 +0800

    solved conflict

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,15 @@
-const mongoose = require("mongoose");
-const url = "mongodb://127.0.0.1:27017/interfaceManagement";
+const mongoose = require('mongoose')
+const url = 'mongodb://127.0.0.1:27017/interfaceManagement'
 
-function connect(){
+function connect() {
   mongoose
-  .connect(url)
-  .then(() => {
-    console.log("Mongodb Connectd...");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect(url)
+    .then(() => {
+      console.log('Mongodb Connectd...')
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
 
 module.exports = connect
-
-
--- src/router/project.js
+++ src/router/project.js
@@ -2,12 +2,36 @@ const Router = require('koa-router')
 
 const router = new Router({ prefix: '/project' })
 
+/**
+ * @api {post} /project/createProject 创建项目
+ * @apiName 创建项目
+ * @apiGroup 项目管理
+ *
+ * @apiBody {String} name 项目名称
+ * @apiBody {String} description 项目描述
+ * @apiBody {Boolean} isPrivate 是否是私有项目
+ *
+ */
 router.post('/createProject', (ctx) => {
   const body = ctx.request.body
   const { name, description, isPrivate } = body
   ctx.body = 'not completed'
 })
 
+/**
+ * @api {post} /project/editProject 修改项目
+ * @apiName 修改项目
+ * @apiGroup 项目管理
+ *
+ * @apiBody {String} projectId 项目ID
+ * @apiBody {String} name 项目名称
+ * @apiBody {String} description 项目描述
+ * @apiBody {Boolean} isPrivate 是否是私有项目
+ * @apiBody {Object[]} userList 成员列表
+ * @apiBody {String} userList.userId 成员ID
+ * @apiBody {string="read","write","admin"} userList.permission 成员权限
+ *
+ */
 router.post('/editProject', (ctx) => {
   const body = ctx.request.body
   const { projectId, name, description, isPrivate, userList } = body
