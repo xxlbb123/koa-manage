2c5678dbbdbdb834392dde049476ef1fb963c9ff
DragonnZhang <731557579@qq.com>
2023-08-01 23:16:59 +0800

    feat: implemented addProject & projectDetail & deleteProject

--- src/router/project.routes.js
+++ src/router/project.routes.js
@@ -1,4 +1,9 @@
 const Router = require('koa-router')
+const jwt = require('jsonwebtoken')
+const { secret } = require('../constant/secretKey')
+const projectModel = require('../models/project')
+const interfaceModel = require('../models/interface')
+
 const router = new Router({ prefix: '/project' })
 /**
  * @api {post} /project/createProject 创建项目
@@ -9,11 +14,32 @@ const router = new Router({ prefix: '/project' })
  * @apiBody {String} description 项目描述
  * @apiBody {Boolean} isPrivate 是否是私有项目
  *
+ * @apiSuccess {String} projectId 创建的项目ID
+ *
  */
-router.post('/createProject', (ctx) => {
+router.post('/createProject', async (ctx) => {
   const body = ctx.request.body
   const { name, description, isPrivate } = body
-  ctx.body = 'not completed'
+
+  const { info } = jwt.verify(ctx.request.headers['token'], secret)
+  const newProject = new projectModel({
+    name,
+    description,
+    created_by: info,
+    members: [
+      {
+        member: info,
+        permission: 0
+      }
+    ],
+    isPrivate
+  })
+  try {
+    const { _id } = await newProject.save()
+    ctx.body = { projectId: _id }
+  } catch (err) {
+    throw err
+  }
 })
 
 /**
@@ -25,14 +51,14 @@ router.post('/createProject', (ctx) => {
  * @apiBody {String} name 项目名称
  * @apiBody {String} description 项目描述
  * @apiBody {Boolean} isPrivate 是否是私有项目
- * @apiBody {Object[]} userList 成员列表
- * @apiBody {String} userList.userId 成员ID
- * @apiBody {string="read","write","admin"} userList.permission 成员权限
+ * @apiBody {Object[]} members 成员列表
+ * @apiBody {String} members.userId 成员ID
+ * @apiBody {string="read","write","admin"} members.permission 成员权限
  *
  */
 router.post('/editProject', (ctx) => {
   const body = ctx.request.body
-  const { projectId, name, description, isPrivate, userList } = body
+  const { projectId, name, description, isPrivate, members } = body
 })
 
 /**
@@ -43,9 +69,15 @@ router.post('/editProject', (ctx) => {
  * @apiBody {String} projectId 项目ID
  *
  */
-router.post('/deleteProject', (ctx) => {
+router.post('/deleteProject', async (ctx) => {
   const body = ctx.request.body
   const { projectId } = body
+
+  try {
+    await projectModel.deleteOne({ _id: projectId })
+  } catch (err) {
+    throw err
+  }
 })
 
 /**
@@ -69,9 +101,18 @@ router.post('/allProjects', (ctx) => {
  * @apiBody {String} projectId 项目ID
  *
  */
-router.post('/projectDetail', (ctx) => {
+router.post('/projectDetail', async (ctx) => {
   const body = ctx.request.body
   const { projectId } = body
+
+  try {
+    const project = await projectModel.findOne({ _id: projectId })
+    const interface = await interfaceModel.findMany({ project: projectId })
+    project.interface = interface
+    ctx.body = project
+  } catch (err) {
+    throw err
+  }
 })
 
 module.exports = router
