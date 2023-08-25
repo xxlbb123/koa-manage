5aa2f9c501ca260ee00a09735325ba9a4586dc5d
wjgsdad <2922788792@qq.com>
2023-08-25 15:58:15 +0800

    feat：修改环境变量导入

--- src/main.js
+++ src/main.js
@@ -2,13 +2,6 @@ const app = require('./app/index')
 
 // 导入环境变量
 const { APP_DEV } = require('./config/config.default')
-// 项目管理相关路由
-// const userModel = require('./models/user')
-// const user = new userModel({
-//   username:"123",
-//   password:"123"`
-// })
-// const userSave=  user.save()
 
 app.listen(APP_DEV, () => {
   console.log(`服务已经启动:http://localhost:${APP_DEV}`)
