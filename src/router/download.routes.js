fe3b0b1a2d974f4b6eccae828f6bc5a88f48a155
xxlbb123 <2922788792@qq.com>
2023-08-27 18:35:21 +0800

    sss

--- src/router/download.routes.js
+++ src/router/download.routes.js
@@ -1,15 +1,18 @@
 const Router = require('koa-router')
 const router = new Router()
 const fs = require('fs')
+const path = require('path')
 router.prefix('/download')
 router.get('/template', async (ctx) => {
   try {
     //  设置下载时的文件名称
-    const fileName = '接口模板.yaml'
-    const filePath = '/www/wwwroot/template/测试实例.yaml' //要下载的yaml文件的路径
-    ctx.attachment(fileName) //设置下载时候的文件名称
+    const templateFilePath = path.join('/www/wwwroot/template', '测试实例.yaml')
+    // 读取文件内容
+    const fileContent = await fs.promises.readFile(templateFilePath)
+    ctx.set('Content-Disposition', 'attachment; filename="测试实例.yaml"')
+
     ctx.type = 'application/x-yaml' //设置下载的文件是yaml类型
-    ctx.body = fs.createReadStream(filePath) //返回文件的内容流
+    ctx.body = fileContent //返回文件的内容流
   } catch (error) {
     ctx.status = 200
     ctx.body = {
